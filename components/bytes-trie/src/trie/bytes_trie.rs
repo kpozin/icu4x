58b3a73c27c81ce71b89c45e8fba2a677952d2e7
Konstantin Pozin <kpozin@google.com>
2020-12-23 20:12:57 -0800

    Bytes trie

---
+++ components/bytes-trie/src/trie/bytes_trie.rs
@@ -0,0 +1,245 @@
+use core::future::poll_fn;
+
+use crate::trie::encoding::*;
+
+pub struct BytesTrie {
+    bytes: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct BytesTrieEntry<'a> {
+    key: &'a [u8],
+    value: Option<i32>,
+}
+
+impl<'a> BytesTrieEntry<'a> {
+    /// The
+    pub fn key(&'a self) -> &'a [u8] {
+        unimplemented!()
+    }
+
+    pub fn key_len(&self) -> usize {
+        self.key().len()
+    }
+
+    /// The value associated with the byte sequence
+    pub fn value(&self) -> i32 {
+        unimplemented!()
+    }
+}
+
+/// Entry in the stack that is built while doing a depth-first traversal of the trie.
+struct StackEntry {
+    /// Position from the beginning of the trie's byte array.
+    position: usize,
+    /// Length of the key (string) from before the node.
+    key_length: usize,
+    /// Remaining length of the branch of the trie.
+    remaining_branch_length: usize,
+}
+
+enum BranchNextResult {
+    /// Final value of a trie branch
+    FinalValue(i32),
+    /// New offset in the byte array after branching
+    NewPosition(usize),
+}
+
+pub struct BytesTrieIterator<'a> {
+    /// Reference to the BytesTrie's contents.
+    bytes: &'a [u8],
+    position: Option<usize>,
+
+    /// The stack stores state entries for backtracking to another outbound edge of a branch node.
+    stack: Vec<StackEntry>,
+
+    key_buffer: Vec<u8>,
+    max_key_length: Option<usize>,
+}
+
+impl<'a> BytesTrieIterator<'a> {
+    fn new() -> Self {
+        todo!()
+    }
+
+    pub fn next(&mut self) -> Option<BytesTrieEntry> {
+        let mut position = match self.position {
+            None => {
+                // Pop the state off the stack and continue with the next outbound edge of the
+                // branch node.
+                let stack_top = self.stack.pop();
+                match stack_top {
+                    None => {
+                        return None;
+                    }
+                    Some(stack_top) => {
+                        self.key_buffer.truncate(stack_top.key_length);
+                        if stack_top.remaining_branch_length > 1 {
+                            match self
+                                .branch_next(stack_top.position, stack_top.remaining_branch_length)
+                            {
+                                BranchNextResult::FinalValue(final_value) => {
+                                    return Some(BytesTrieEntry {
+                                        key: self.key_bytes(),
+                                        value: Some(final_value),
+                                    });
+                                }
+                                BranchNextResult::NewPosition(new_position) => new_position,
+                            }
+                        } else {
+                            self.key_buffer.push(self.bytes[stack_top.position]);
+                            stack_top.position + 1
+                        }
+                    }
+                }
+            }
+            Some(current_position) => current_position,
+        };
+
+        // Diff from Java/CPP: Not porting remaining_match_length.
+
+        loop {
+            let mut node = self.bytes[position];
+            position += 1;
+
+            if node >= CompactValue::MIN_LEAD {
+                // Deliver value for the byte sequence so far.
+                let is_final = CompactValue::is_final(node);
+                let value = CompactValue::read_int(&self.bytes, position);
+                if is_final || self.is_at_max_key_length() {
+                    self.position = None;
+                } else {
+                    self.position = Some(position + CompactValue::get_width(&self.bytes, position));
+                }
+                return Some(BytesTrieEntry {
+                    key: self.key_bytes(),
+                    value: Some(value),
+                });
+            }
+
+            if self.is_at_max_key_length() {
+                return Some(self.truncate_and_stop());
+            }
+
+            if node < MIN_LINEAR_MATCH {
+                if node == 0 {
+                    node = self.bytes[position];
+                    position += 1;
+                }
+                position = match self.branch_next(position, (node + 1) as usize) {
+                    BranchNextResult::FinalValue(final_value) => {
+                        return Some(BytesTrieEntry {
+                            key: self.key_bytes(),
+                            value: Some(final_value),
+                        });
+                    }
+                    BranchNextResult::NewPosition(new_position) => new_position,
+                };
+            } else /* if node >= MIN_LINEAR_MATCH */ {
+                // Linear-match node, append `added_length` bytes to buffer.
+                let added_length = (node - MIN_LINEAR_MATCH + 1) as usize;
+                match self.max_key_length {
+                    Some(max_key_length) if self.key_length() + added_length > max_key_length => {
+                        self.key_buffer.extend_from_slice(
+                            &self.bytes[position..position + max_key_length - self.key_length()],
+                        );
+                        return Some(self.truncate_and_stop());
+                    }
+                    _ => {
+                        self.key_buffer
+                            .extend_from_slice(&self.bytes[position..position + added_length]);
+                        position += added_length;
+                    }
+                }
+            }
+        }
+    }
+
+    fn branch_next(
+        &mut self,
+        mut position: usize,
+        mut remaining_branch_length: usize,
+    ) -> BranchNextResult {
+        while remaining_branch_length > MAX_BRANCH_LINEAR_SUB_NODE_LENGTH as usize {
+            // Ignore the comparison byte
+            // TODO(kpozin): What comparison byte?
+            position += 1;
+            // Push state for the greater-or-equal edge.
+            {
+                let saved_position = BytesTrieIterator::skip_delta(self.bytes, position);
+                let saved_remaining_branch_length =
+                    remaining_branch_length - (remaining_branch_length >> 1);
+                let saved_key_length = self.key_buffer.len();
+                self.push_state(
+                    saved_position,
+                    saved_remaining_branch_length,
+                    saved_key_length,
+                );
+            }
+            // Follow the less-than edge.
+            remaining_branch_length >>= 1;
+            position = BytesTrieIterator::jump_by_delta(self.bytes, position);
+        }
+
+        // List of key-value pairs where values are either final values or jump deltas.
+        // Read the first (key, value) pair.
+
+        let key_byte = self.bytes[position];
+        position += 1;
+        let node = self.bytes[position];
+        let is_final = CompactValue::is_final(node);
+
+        let value = CompactValue::read_int(self.bytes, position);
+        position += CompactValue::get_width(self.bytes, position);
+        self.push_state(position, remaining_branch_length - 1, self.key_buffer.len());
+        self.key_buffer.push(key_byte);
+
+        if is_final {
+            BranchNextResult::FinalValue(value)
+        } else {
+            BranchNextResult::NewPosition(position + value as usize)
+        }
+    }
+
+    fn push_state(&mut self, position: usize, remaining_branch_length: usize, key_length: usize) {
+        self.stack.push(StackEntry {
+            position,
+            key_length,
+            remaining_branch_length,
+        })
+    }
+
+    fn key_bytes(&self) -> &[u8] {
+        self.key_buffer.as_slice()
+    }
+
+    fn key_length(&self) -> usize {
+        self.key_buffer.len()
+    }
+
+    fn is_at_max_key_length(&self) -> bool {
+        match self.max_key_length {
+            None => false,
+            Some(max_length) => max_length == self.key_length(),
+        }
+    }
+
+    fn truncate_and_stop(&mut self) -> BytesTrieEntry {
+        self.position = None;
+        return BytesTrieEntry {
+            key: self.key_bytes(),
+            value: None,
+        };
+    }
+
+    fn skip_delta(bytes: &[u8], offset: usize) -> usize {
+        offset + CompactDelta::get_width(bytes, offset) as usize
+    }
+
+    fn jump_by_delta(bytes: &[u8], offset: usize) -> usize {
+        let delta = CompactDelta::read_int(bytes, offset);
+        assert!(delta >= 0);
+        let delta_width_bytes = CompactDelta::get_width(bytes, offset);
+        offset + delta_width_bytes + delta as usize
+    }
+}
---
+++ components/bytes-trie/src/trie/encoding.rs
@@ -0,0 +1,150 @@
+use std::convert::TryInto;
+
+// Node lead byte values.
+
+// 00..0f: Branch node. If node!=0 then the length is node+1, otherwise
+// the length is one more than the next byte.
+
+// For a branch sub-node with at most this many entries, we drop down
+// to a linear search.
+pub(crate) const MAX_BRANCH_LINEAR_SUB_NODE_LENGTH: u8 = 5;
+
+// 10..1f: Linear-match node, match 1..16 bytes and continue reading the next node.
+pub(crate) const MIN_LINEAR_MATCH: u8 = 0x10;
+pub(crate) const MAX_LINEAR_MATCH_LENGTH: u8 = 0x10;
+
+// 20..ff: Variable-length value node.
+// If odd, the value is final. (Otherwise, intermediate value or jump delta.)
+// Then shift-right by 1 bit.
+// The remaining lead byte value indicates the number of following bytes (0..4)
+// and contains the value's top bits.
+
+/// It is a final value if bit 0 is set.
+pub(crate) const VALUE_IS_FINAL: u8 = 0x1;
+
+pub(crate) trait CompactInt {
+    const RIGHT_SHIFT: u8;
+
+    const MAX_ONE_BYTE: u8;
+    const MAX_TWO_BYTE: i32;
+    const MAX_THREE_BYTE: i32;
+
+    const MIN_ONE_BYTE_LEAD: u8;
+    const MIN_TWO_BYTE_LEAD: u8;
+    const MIN_THREE_BYTE_LEAD: u8;
+    const FOUR_BYTE_LEAD: u8;
+    const FIVE_BYTE_LEAD: u8;
+
+    /// Reads the compact integer starting at the given position in the byte array.
+    fn read_int(bytes: &[u8], offset: usize) -> i32 {
+        let lead_byte: u8 = bytes[offset] >> Self::RIGHT_SHIFT;
+        if lead_byte < Self::MIN_TWO_BYTE_LEAD {
+            (lead_byte - Self::MIN_ONE_BYTE_LEAD) as i32
+        } else if lead_byte < Self::MIN_THREE_BYTE_LEAD {
+            ((lead_byte as i32 - Self::MIN_TWO_BYTE_LEAD as i32) << 8) | (bytes[offset + 1] as i32)
+        } else if lead_byte < Self::FOUR_BYTE_LEAD {
+            ((lead_byte as i32 - Self::MIN_THREE_BYTE_LEAD as i32) << 16)
+                | ((bytes[offset + 1] as i32) << 8)
+                | (bytes[offset + 2] as i32)
+        } else if lead_byte == Self::FOUR_BYTE_LEAD {
+            ((bytes[offset + 1] as i32) << 16)
+                | ((bytes[offset + 2] as i32) << 8)
+                | (bytes[offset + 3] as i32)
+        } else {
+            ((bytes[offset + 1] as i32) << 24)
+                | ((bytes[offset + 2] as i32) << 16)
+                | ((bytes[offset + 3] as i32) << 8)
+                | (bytes[offset + 4] as i32)
+        }
+    }
+
+    /// Calculates the width in bytes of the compact integer starting at the given position in the
+    /// byte array.
+    fn get_width(bytes: &[u8], offset: usize) -> usize;
+}
+
+/// Compact value: After testing bit 0, shift right by 1 and then use the following thresholds.
+pub(crate) struct CompactValue {}
+
+impl CompactValue {
+    /// 0x20
+    pub(crate) const MIN_LEAD: u8 = MIN_LINEAR_MATCH + MAX_LINEAR_MATCH_LENGTH;
+
+    pub(crate) fn is_final(byte: u8) -> bool {
+        (byte & VALUE_IS_FINAL as u8) != 0
+    }
+}
+
+impl CompactInt for CompactValue {
+    const RIGHT_SHIFT: u8 = 1;
+
+    /// At least 6 bits in the first byte.
+    const MAX_ONE_BYTE: u8 = 0x40;
+    const MAX_TWO_BYTE: i32 = 0x1aff;
+    /// A little more than Unicode code points. (0x11ffff)
+    const MAX_THREE_BYTE: i32 =
+        ((Self::FOUR_BYTE_LEAD as i32 - Self::MIN_THREE_BYTE_LEAD as i32) << 16) - 1;
+
+    /// 0x10
+    const MIN_ONE_BYTE_LEAD: u8 = Self::MIN_LEAD / 2;
+    /// 0x51
+    const MIN_TWO_BYTE_LEAD: u8 = Self::MIN_ONE_BYTE_LEAD + Self::MAX_ONE_BYTE;
+    /// 0x6c
+    const MIN_THREE_BYTE_LEAD: u8 = Self::MIN_TWO_BYTE_LEAD + (Self::MAX_TWO_BYTE >> 8) as u8 + 1;
+    const FOUR_BYTE_LEAD: u8 = 0x7e;
+    const FIVE_BYTE_LEAD: u8 = 0x7f;
+
+    fn get_width(bytes: &[u8], offset: usize) -> usize {
+        let lead_byte = bytes[offset];
+        assert!(lead_byte >= Self::MIN_LEAD);
+
+        if lead_byte < (Self::MIN_TWO_BYTE_LEAD << Self::RIGHT_SHIFT) {
+            1
+        } else if lead_byte < (Self::MIN_THREE_BYTE_LEAD << Self::RIGHT_SHIFT) {
+            2
+        } else if lead_byte < (Self::FOUR_BYTE_LEAD << Self::RIGHT_SHIFT) {
+            3
+        } else {
+            (4 + ((lead_byte >> Self::RIGHT_SHIFT) & 1))
+                .try_into()
+                .unwrap()
+        }
+    }
+}
+
+/// Compact delta integers.
+pub(crate) struct CompactDelta {}
+
+impl CompactDelta {}
+
+impl CompactInt for CompactDelta {
+    const RIGHT_SHIFT: u8 = 0;
+
+    const MAX_ONE_BYTE: u8 = 0xbf;
+    /// 0x2fff
+    const MAX_TWO_BYTE: i32 =
+        ((Self::MIN_THREE_BYTE_LEAD as i32 - Self::MIN_TWO_BYTE_LEAD as i32) << 8) - 1;
+    const MAX_THREE_BYTE: i32 =
+        ((Self::FOUR_BYTE_LEAD as i32 - Self::MIN_THREE_BYTE_LEAD as i32) << 16) - 1;
+
+    /// Does not apply for `CompactDelta`.
+    const MIN_ONE_BYTE_LEAD: u8 = 0;
+    /// 0xc0
+    const MIN_TWO_BYTE_LEAD: u8 = Self::MAX_ONE_BYTE + 1;
+    const MIN_THREE_BYTE_LEAD: u8 = 0xf0;
+    const FOUR_BYTE_LEAD: u8 = 0xfe;
+    const FIVE_BYTE_LEAD: u8 = 0xff;
+
+    fn get_width(bytes: &[u8], offset: usize) -> usize {
+        let lead_byte = bytes[offset];
+        if lead_byte < Self::MIN_TWO_BYTE_LEAD {
+            1
+        } else if lead_byte < Self::MIN_THREE_BYTE_LEAD {
+            2
+        } else if lead_byte < Self::FOUR_BYTE_LEAD {
+            3
+        } else {
+            (4 + (lead_byte & 1)) as usize
+        }
+    }
+}
